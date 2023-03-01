223e3d47c0b197569cef67d23e78d092387e5fff
edisson89 <edisson.12@hotmail.com>
2023-02-28 20:32:52 -0500

    revisando rutas

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/",
+  base:" https://edisson89.github.io/test-productos/",
   plugins: [react()],
 })
